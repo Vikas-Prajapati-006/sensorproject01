8f3700c51284022aab8f35634808f8df48ffe9af
Vikas Prajapati <vikasprajapati.1653@gmail.com>
2025-09-22 12:34:52 +0530

    Reapply "completed all modules"
    This reverts commit ce2336256cae3fc2a372c5e38886ec5bb6b8f576.

---
+++ src/constant/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "wafer-fault"
+MONGO_DATABASE_NAME = "ml_project"
+MONGO_COLLECTION_NAME = "waferfault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://Vikas:FHs98rBBpN65GxzX@cluster0.hdn9wuj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
